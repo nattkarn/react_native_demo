a0a8047672312c198ed2ffae75339420c0bde830
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-07-03 12:58:28 +0700

    update not go black to splashscreen

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -1,29 +1,32 @@
-import { View, Text, Image, Pressable, Alert } from "react-native"
-import {splashStyle} from '../Styles/SplashStyle'
+import { useEffect } from "react";
+import { View, Text, Image } from "react-native";
+import { CommonActions } from '@react-navigation/native';
+import { splashStyle } from '../Styles/SplashStyle';
 
-const logo = require("../assets/catLogo.png")
+const logo = require("../assets/catLogo.png");
 
 export default function SplashScreen({ navigation }) {
-    const onPress = () =>{
-        navigation.navigate('Home')
-    }
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.dispatch(
+                CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: 'Home' }],
+                })
+            );
+        }, 2000); // 2000 milliseconds = 2 seconds
+
+        return () => clearTimeout(timer); // Clean up the timer on component unmount
+    }, [navigation]);
     
     return (
-        <Pressable onPress={onPress}>
-            <View style={splashStyle.splash}>
-                <View style={splashStyle.splashLogo}>
-                    <Image source={logo} style = {splashStyle.splashImage}></Image>
-                </View>
-                <View style={splashStyle.splashContent}>
-                    <Text style={splashStyle.splashText}>Cat Coder</Text>
-                </View>
+        <View style={splashStyle.splash}>
+            <View style={splashStyle.splashLogo}>
+                <Image source={logo} style={splashStyle.splashImage} />
             </View>
-        </Pressable>
-    )
+            <View style={splashStyle.splashContent}>
+                <Text style={splashStyle.splashText}>Cat Coder</Text>
+            </View>
+        </View>
+    );
 }
-
-
-
-  <Text>I'm pressable!</Text>
-
-
