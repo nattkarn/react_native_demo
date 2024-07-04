513971424c9f679bbd34194ff20e38036fd8b4c9
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-07-04 15:44:30 +0700

    Fix bug

---
+++ Styles/AttractionsStyle.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+export const attractionStyle = StyleSheet.create({
+  container: {
+    paddingVertical: 50,
+    paddingHorizontal:10
+  },
+  card:{
+    marginVertical:10
+  },
+  textHeader:{
+    textAlign:'center',
+    fontSize:40,
+    fontWeight:'bold'
+  },
+  image: {
+    width: "100%",
+    height: 333,
+  },
+  imageTextHeader:{
+    fontSize:20,
+    fontWeight:'bold',
+  },
+  imageText:{
+    fontSize:20,
+    marginTop:10
+  },
+  textDetail:{
+    fontSize:15,
+    paddingBottom:15
+  }
+});
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,73 +1,30 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  Image,
-  Pressable,
-  ActivityIndicator,
-} from "react-native";
-import { Searchbar } from "react-native-paper";
+import { View, Text, ScrollView, Pressable } from "react-native";
 import { homeStyle } from "../../Styles/HomeStyle";
 
-import React, { useEffect, useState } from "react";
+import { Avatar } from "react-native-paper";
 
 export default function HomeScreen({ navigation }) {
-  const onPressItem = (id, name) => {
-    navigation.navigate("Detail", { id: id, name: name });
+  const onPressAttractions = () => {
+    navigation.navigate("Attractions");
   };
 
-  const [isLoading, setLoading] = useState(true);
-  const [items, setItems] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const getAttractions = async (query = "") => {
-    setLoading(true);
-    try {
-      const response = await fetch(
-        `https://www.melivecode.com/api/attractions?search=${query}`
-      );
-      const json = await response.json();
-      setItems(json);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getAttractions();
-  }, []);
-
-  const handleSearchChange = (query) => {
-    setSearchQuery(query);
-    getAttractions(query);
+  const onPressLogin = () => {
+    navigation.navigate("Login");
   };
 
   return (
     <View style={homeStyle.container}>
-      <Searchbar
-        label="ค้นหาสถานที่"
-        value={searchQuery}
-        onChangeText={handleSearchChange}
-        style={{ margin: 10 }}
-      />
-      {isLoading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <ScrollView>
-          <Text style={homeStyle.textHeader}>Attractions</Text>
-          {items.map((d) => (
-            <View style={homeStyle.card} key={d.id}>
-              <Pressable onPress={() => onPressItem(d.id, d.name.toString())}>
-                <Image source={{ uri: d.coverimage }} style={homeStyle.image} />
-                <Text style={homeStyle.imageTextHeader}>{d.name}</Text>
-                <Text style={homeStyle.textDetail}>{d.detail}</Text>
-              </Pressable>
-            </View>
-          ))}
-        </ScrollView>
-      )}
+      <ScrollView>
+        <Text style={homeStyle.textHeader}>Home</Text>
+        <View style={homeStyle.containerGrid}>
+          <Pressable onPress={() => onPressAttractions()}>
+            <Avatar.Icon size={150} icon="airplane-takeoff" />
+          </Pressable>
+          <Pressable onPress={() => onPressLogin()}>
+            <Avatar.Icon size={150} icon="login" />
+          </Pressable>
+        </View>
+      </ScrollView>
     </View>
   );
 }
